d741ccffae891b6614da8195f933d309f36d0e03
Fabian Holtkötter <fabianholtkoetter@gmail.com>
2016-04-20 11:12:58 +0200

    closes #5 closes #6 (#7)

--- Aufgabe3.3 Philosophen/src/main/java/DiningTable.java
+++ Aufgabe3.3 Philosophen/src/main/java/DiningTable.java
@@ -13,66 +13,68 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class DiningTable {
 
-  private final ReentrantLock[] forks;
-  private Random rGenerator = ThreadLocalRandom.current();
+    private final ReentrantLock[] forks;
+    private ThreadLocalRandom rGenerator = ThreadLocalRandom.current();
 
-  public DiningTable(int seats) {
+    public DiningTable(int seats) {
 
-    this.forks = new ReentrantLock[(seats != 1) ? seats : 2];
+        this.forks = new ReentrantLock[(seats != 1) ? seats : 2];
 
-    for (int i = 0; i < forks.length; i++) {
-      this.forks[i] = new ReentrantLock();
+        for (int i = 0; i < forks.length; i++) {
+            this.forks[i] = new ReentrantLock();
+        }
     }
-  }
 
-  public void takeSeat(Philosopher philosopher) throws InterruptedException {
+    public void takeSeat(Philosopher philosopher) throws InterruptedException {
 
-    while (!Thread.currentThread().isInterrupted()) {
+        while (!Thread.currentThread().isInterrupted()) {
 
-      //Philosopher runs around the table till and searches for free forks
-      for (int i = 0; i < forks.length; i++) {
+            int startVal = rGenerator.nextInt(0, forks.length);
 
-        //Set index of left and right fork
-        int left = i;
-        int right = (i + 1) % forks.length;
-        boolean rand = rGenerator.nextBoolean();
+            //Philosopher runs around the table till and searches for free forks
+            for (int i = startVal; i != (startVal - 1) % forks.length; i = (i + 1) % forks.length) {
 
-        //Try to get the forks
-        if (((rand) ? forks[left].tryLock() : forks[right].tryLock()) &&
-            ((!rand) ? forks[left].tryLock() : forks[right].tryLock())) {
+                //Set index of left and right fork
+                int left = i;
+                int right = (i + 1) % forks.length;
+                boolean rand = rGenerator.nextBoolean();
 
-          philosopher.takeAll(left, right);
+                //Try to get the forks
+                if (((rand) ? forks[left].tryLock() : forks[right].tryLock()) &&
+                        ((!rand) ? forks[left].tryLock() : forks[right].tryLock())) {
 
-          return;
+                    philosopher.takeAll(left, right);
 
-        } else {
-          //Drop forks if held
-          if (forks[left].isHeldByCurrentThread())
-            forks[left].unlock();
+                    return;
 
-          if (forks[right].isHeldByCurrentThread())
-            forks[right].unlock();
+                } else {
+                    //Drop forks if held
+                    if (forks[left].isHeldByCurrentThread())
+                        forks[left].unlock();
+
+                    if (forks[right].isHeldByCurrentThread())
+                        forks[right].unlock();
+                }
+
+            }
+
+            //Wait till a seat gets free
+            synchronized (forks[startVal]) {
+                forks[startVal].wait();
+            }
+        }
+    }
+
+    public void leaveSeat(int seatNumber) {
+        //Release forks
+        forks[seatNumber].unlock();
+        forks[(seatNumber + 1) % forks.length].unlock();
+
+        //Notify Waiting philosophers
+        synchronized (forks[seatNumber]) {
+            forks[seatNumber].notify();
         }
 
-      }
 
-      //Wait till a seat gets free
-      synchronized (forks){
-          forks.wait();
-      }
     }
-  }
-
-  public void leaveSeat(int seatNumber) {
-    //Release forks
-    forks[seatNumber].unlock();
-    forks[(seatNumber + 1) % forks.length].unlock();
-
-    //Notify Waiting philosophers
-    synchronized (forks) {
-      forks.notify();
-    }
-
-
-  }
 }
