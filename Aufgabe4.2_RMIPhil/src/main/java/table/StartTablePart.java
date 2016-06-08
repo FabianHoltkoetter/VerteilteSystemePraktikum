851996588a101a93a3a48608e5af3a7ed3d78321
René Zarwel <darenegade@gmx.de>
2016-06-08 11:20:15 +0200

    TP Starter mit IP in args

--- Aufgabe4.2_RMIPhil/src/main/java/table/StartTablePart.java
+++ Aufgabe4.2_RMIPhil/src/main/java/table/StartTablePart.java
@@ -1,10 +1,23 @@
 package table;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Created by Fabian on 08.06.2016.
  */
 public class StartTablePart {
+    private static final Logger LOG = Logger.getLogger(StartTablePart.class.getName());
+
+
     public static void main(String[] args) {
-        new TablePartImpl("localhost");
+
+        if(args.length == 1) {
+
+            new TablePartImpl(args[0]);
+
+        } else {
+            LOG.log(Level.INFO, "No IP provided in args");
+        }
     }
 }
