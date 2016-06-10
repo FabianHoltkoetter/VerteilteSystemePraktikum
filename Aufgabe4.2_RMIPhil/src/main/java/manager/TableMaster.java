0b6818f804edaecf1f484074a9c771cadc4d3253
René Zarwel <darenegade@gmx.de>
2016-06-10 11:20:11 +0200

    #13 TableMaster started + Recovery Phil

--- Aufgabe4.2_RMIPhil/src/main/java/manager/TableMaster.java
+++ Aufgabe4.2_RMIPhil/src/main/java/manager/TableMaster.java
@@ -14,61 +14,66 @@ import java.util.logging.Logger;
  * Created by Fabian on 08.06.2016.
  */
 public class TableMaster extends Thread {
-    private static final Logger LOG = Logger.getLogger(TableMaster.class.getName());
-    private static final int PAUSE = 50;
+  private static final Logger LOG = Logger.getLogger(TableMaster.class.getName());
+  private static final int PAUSE = 5000;
 
-    private int avgEatCout;
-    private final Manager manager;
+  private int avgEatCout;
+  private final Manager manager;
 
-    public TableMaster() {
-        try {
-            Registry registry = LocateRegistry.getRegistry();
-            manager = (Manager) registry.lookup(Manager.NAME);
-            LOG.info("TableMaster received Manager.");
-        } catch (Exception e) {
-            LOG.severe("Error in TableMaster.");
-            throw new RuntimeException(e.getMessage());
-        }
+  public TableMaster() {
+    try {
+      Registry registry = LocateRegistry.getRegistry();
+      manager = (Manager) registry.lookup(Manager.NAME);
+      LOG.info("TableMaster received Manager.");
+    } catch (Exception e) {
+      LOG.severe("Error in TableMaster.");
+      throw new RuntimeException(e.getMessage());
     }
+  }
 
-    @Override
-    public void run() {
-        LOG.log(Level.INFO, "TableMaster started!");
-        while (!isInterrupted()) {
-            try {
-                //Calculate average eat count
-                List<Philosopher> philosophers = manager.getPhilosophers();
-                LOG.info(String.format("Manager received %d philosophers.", philosophers.size()));
-                philosophers.forEach(philosopher -> {
-                            try {
-                                avgEatCout += philosopher.getEatCounter();
-                            } catch (RemoteException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                );
+  @Override
+  public void run() {
+    LOG.log(Level.INFO, "TableMaster started!");
+    while (!isInterrupted()) {
+      try {
+        Thread.sleep(PAUSE);
 
-                avgEatCout /= philosophers.size();
-                LOG.info(String.format("Average eat count is %d", avgEatCout));
+        //Calculate average eat count
+        List<Philosopher> philosophers = manager.getPhilosophers();
 
-                //Allow or forbid philosopher to eat
-                philosophers.forEach(p -> {
-                    try {
-                        p.setAllowedToEat(p.getEatCounter() < avgEatCout * 1.1);
-                    } catch (RemoteException e) {
-                        e.printStackTrace();
-                    }
-                });
+        LOG.info(String.format("TableMaster woke up and received %d philosophers.", philosophers.size()));
 
-                avgEatCout = 0;
+        if(philosophers.size() == 0)
+          continue;
 
-                Thread.sleep(PAUSE);
-            } catch (Exception e) {
+        philosophers.forEach(philosopher -> {
+              try {
+                avgEatCout += philosopher.getEatCounter();
+              } catch (RemoteException e) {
                 e.printStackTrace();
-                LOG.severe("Error in TableMaster!");
-                break;
+              }
             }
-        }
+        );
+
+        avgEatCout /= philosophers.size();
+        LOG.info(String.format("Average eat count is %d", avgEatCout));
+
+        //Allow or forbid philosopher to eat
+        philosophers.forEach(p -> {
+          try {
+            p.setAllowedToEat(p.getEatCounter() < avgEatCout * 1.1);
+          } catch (RemoteException e) {
+            e.printStackTrace();
+          }
+        });
+
+        avgEatCout = 0;
+
+      } catch (Exception e) {
+        e.printStackTrace();
+        LOG.severe("Error in TableMaster!");
+      }
     }
+  }
 
 }
